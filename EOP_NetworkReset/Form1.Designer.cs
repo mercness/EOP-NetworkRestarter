6addaec180075359cccf7ba5aa218bd13b0252e8
mercness <stephan@baronco.com.au>
2018-03-25 19:20:57 +1000

    Initial upload
    Base working application - determines if IP is reachable via ping, if not it asks the user if they want to restart their NIC

---
+++ EOP_NetworkReset/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace EOP_NetworkReset
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "8.8.8.8";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(144, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(112, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Ping Automatically";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 223);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(349, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 245);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Ethernet Over Power - Network Monitor";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ EOP_NetworkReset/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace EOP_NetworkReset
+{
+    public partial class FrmMain : Form
+    {
+
+        public FrmMain()
+        {
+            InitializeComponent();        
+        }
+
+            private void FrmMain_Load(object sender, EventArgs e)
+            {
+                textBox1.Enabled = false;
+                toolStripStatusLabel1.Text = "Error Count: 0";
+            }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int error_count;
+            error_count = 0;
+            toolStripStatusLabel1.Text = "Error Count: "+error_count.ToString();
+
+                try
+                    {
+                        Ping p = new Ping();
+                        PingReply r;
+                        string s;
+
+                        s = textBox1.Text;
+                        r = p.Send(s);
+                        label1.Text = "Ping to " + s.ToString() + "[" + r.Address.ToString() + "]" + " Successful"
+                                         + " Response delay = " + r.RoundtripTime.ToString() + " ms" + "\n";
+                    }
+                catch (PingException)
+                    {
+
+                        // increase error count by 1, update the count on screen, show  message, stop the timer
+                        error_count = error_count + 1;
+                        timer1.Enabled = false;
+                        checkBox1.Checked = false;
+                        toolStripStatusLabel1.Text = "Error Count: " + error_count.ToString();
+                        DialogResult pingerror = MessageBox.Show("Should we restart NIC?","Unable to ping "+textBox1.Text, MessageBoxButtons.YesNo);
+
+                            if (pingerror == DialogResult.Yes)
+                                {
+                                // Do something
+                                MessageBox.Show("Okay restarting NIC");
+                                }
+                            else if (pingerror == DialogResult.No)
+                                {
+                                    MessageBox.Show("Okay I won't restart the NIC");
+                                }
+                    }
+            }
+            
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+                button1_Click(sender, e);
+
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox1.Enabled = false;
+                textBox1.Update();
+                timer1.Enabled = true;
+            }
+
+            if (checkBox1.Checked == false)
+            {
+                textBox1.Enabled = true;
+                textBox1.Update();
+                timer1.Enabled = false;
+            }
+        }
+    }
+
+}
