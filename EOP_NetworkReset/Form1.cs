bab0200708e7c3337e07a5f69633a68d114b5c14
mercness <stephan@baronco.com.au>
2018-03-26 00:24:24 +1000

    Added Chart Control
    Chart Control now shows the pings in a series where x is the ping # and y is the ms response time

--- EOP_NetworkReset/Form1.cs
+++ EOP_NetworkReset/Form1.cs
@@ -19,7 +19,8 @@ namespace EOP_NetworkReset
 
         public FrmMain()
         {
-            InitializeComponent();        
+            InitializeComponent();
+
         }
 
             private void FrmMain_Load(object sender, EventArgs e)
@@ -28,19 +29,36 @@ namespace EOP_NetworkReset
 
                 gatewayip = GetDefaultGateway();
                 textBox1.Text = gatewayip.ToString();
+                StaticValues.error_count = 0;
+                StaticValues.pingnumber = 0;
 
                 textBox1.Enabled = false;
                 toolStripStatusLabel1.Text = "Error Count: 0";
-            }
 
+                    List<string> list = new List<string>();
+                        NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+                        foreach (NetworkInterface adapter in adapters)
+                            {
+                                list.Add(adapter.Description);
+                            }
+
+            comboBox1.DataSource = list;
+
+        }
+
+        public static class StaticValues
+        {
+            public static int error_count { get; set; }
+            public static int pingnumber { get; set; }
+
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int error_count;
-            error_count = 0;
-            toolStripStatusLabel1.Text = "Error Count: "+error_count.ToString();
 
-                try
+            toolStripStatusLabel1.Text = "Error Count: "+StaticValues.error_count.ToString();
+
+            try
                     {
                         Ping p = new Ping();
                         PingReply r;
@@ -50,15 +68,26 @@ namespace EOP_NetworkReset
                         r = p.Send(s);
                         label1.Text = "Ping to " + s.ToString() + "[" + r.Address.ToString() + "]" + " Successful"
                                          + " Response delay = " + r.RoundtripTime.ToString() + " ms" + "\n";
-                    }
+
+
+                        //List<string> pingms = new List<string>();
+                        //{
+                        //    pingms.Add(r.RoundtripTime.ToString());
+                        //}
+
+                
+                chart1.Series["Ping"].Points.AddXY(StaticValues.pingnumber.ToString(), r.RoundtripTime.ToString());
+                StaticValues.pingnumber = StaticValues.pingnumber + 1;
+
+            }
                 catch (PingException)
                     {
 
-                        // increase error count by 1, update the count on screen, show  message, stop the timer
-                        error_count = error_count + 1;
+                // increase error count by 1, update the count on screen, show  message, stop the timer
+                        StaticValues.error_count = StaticValues.error_count + 1;
                         timer1.Enabled = false;
                         checkBox1.Checked = false;
-                        toolStripStatusLabel1.Text = "Error Count: " + error_count.ToString();
+                        toolStripStatusLabel1.Text = "Error Count: " + StaticValues.error_count.ToString();
                         DialogResult pingerror = MessageBox.Show("Should we restart NIC?","Unable to ping "+textBox1.Text, MessageBoxButtons.YesNo);
 
                             if (pingerror == DialogResult.Yes)
